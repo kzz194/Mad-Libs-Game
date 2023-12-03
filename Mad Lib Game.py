04d649e9054b5b7ab568e3ba0a99941a938b66b8
zzz194 <moneymaking14223@gmail.com>
2023-12-03 18:55:45 -0500

    Initial commit of Python Mad Lib Game

---
+++ Mad Lib Game.py
@@ -0,0 +1,7 @@
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+Adjective = input("Enter a Adjective: ")
+
+print("Roses are {color}")
+print("{plural noun} are Green")
+print("You {Adjective}")
