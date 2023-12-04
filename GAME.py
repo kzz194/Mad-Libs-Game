1e102858bba22acf78f64269d81df052bc643cc7
zzz194 <131400468+zzz194@users.noreply.github.com>
2023-12-03 19:50:21 -0500

    Add files via upload

---
+++ GAME.py
@@ -0,0 +1,21 @@
+#color = input("Enter a color: ")
+#plural_noun = input("Enter a plural noun: ")
+#Adjective = input("Enter a Adjective: ")
+
+#print("Roses are " +color)
+#print(plural_noun + " are Green")
+#print("You " + Adjective)
+
+Type_of_friend_adjective = input("Enter a adjective: ")  #1
+place = input("Enter a place: ")                         #2
+person_or_character = input("Enter a person or character: ")  #3
+verb = input("Enter a verb: ")                                #4
+Noun = input("Enter a Noun: ")                                #5
+Adjective = input("Enter a Adjective: ")                      #6
+Type_of_food = input("Enter a type of food: ")                #7
+Type_of_place = input("Enter a type of place: ")              #8
+
+
+print("One day, my " + Type_of_friend_adjective + " friend and I decided to go to the " + place + ".")
+print("We really wanted to " + verb + " the " + Noun + " which everyone said was very " + Adjective + ".")
+print("After that, we went to grab some " + Type_of_food + " at the nearest " + Type_of_place + " and had a great day.")
